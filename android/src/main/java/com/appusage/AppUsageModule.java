4be8e0da48e1c32d0b24f4242c6c2c7fd134fec3
Hassan Ali <hali8629@gmail.com>
2023-05-15 17:46:49 +0500

    chore: initial commit

---
+++ android/src/main/java/com/appusage/AppUsageModule.java
@@ -0,0 +1,32 @@
+package com.appusage;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = AppUsageModule.NAME)
+public class AppUsageModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "AppUsage";
+
+  public AppUsageModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  AppUsageExample
+//
+
+import Foundation
